826b2124efea12e83402c04fc05ac22bdfdb082a
MartkCz <antik494@gmail.com>
2019-04-05 10:54:41 +0200

    nette di schema

--- src/DI/AssetsBuilderExtension.php
+++ src/DI/AssetsBuilderExtension.php
@@ -3,38 +3,47 @@
 namespace WebChemistry\AssetsBuilder\DI;
 
 use Nette\DI\CompilerExtension;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 use WebChemistry\AssetsBuilder\AssetsBuilder;
 use WebChemistry\AssetsBuilder\Nonce\INonceProvider;
 use WebChemistry\AssetsBuilder\Nonce\NonceProvider;
 
 final class AssetsBuilderExtension extends CompilerExtension {
 
-	/** @var mixed[] */
-	public $defaults = [
-		'css' => [],
-		'js' => [],
-		'manifests' => [],
-		'nonceProvider' => NonceProvider::class
-	];
+	public function getConfigSchema(): Schema {
+		return Expect::structure([
+			'css' => Expect::arrayOf('string')->default([]),
+			'js' => Expect::arrayOf('string')->default([]),
+			'manifests' => Expect::arrayOf(
+				Expect::structure([
+					'manifest' => Expect::string(),
+					'styles' => Expect::arrayOf('string')->default([]),
+					'javascript' => Expect::arrayOf('string')->default([]),
+				])
+			),
+			'nonceProvider' => Expect::string(NonceProvider::class),
+		]);
+	}
 
 	public function loadConfiguration(): void {
 		$builder = $this->getContainerBuilder();
-		$config = $this->validateConfig($this->defaults);
+		$config = $this->getConfig();
 
 		$builder->addDefinition($this->prefix('nonceProvider'))
 			->setType(INonceProvider::class)
-			->setFactory($config['nonceProvider']);
+			->setFactory($config->nonceProvider);
 
 		$assets = $builder->addDefinition($this->prefix('assetsBuilder'))
 			->setFactory(AssetsBuilder::class);
 
-		foreach ($config['manifests'] as $name => $options) {
-			$assets->addSetup('addManifest', [$name, $options['manifest'], $options['styles'], $options['javascript']]);
+		foreach ($config->manifests as $name => $options) {
+			$assets->addSetup('addManifest', [$name, $options->manifest, $options->styles, $options->javascript]);
 		}
-		foreach ($config['css'] as $css) {
+		foreach ($config->css as $css) {
 			$assets->addSetup('addCss', [$css]);
 		}
-		foreach ($config['js'] as $js) {
+		foreach ($config->js as $js) {
 			$assets->addSetup('addJs', [$js]);
 		}
 	}
